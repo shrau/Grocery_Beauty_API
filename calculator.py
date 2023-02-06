b120d78348792f206b8129174f51fd97451127f8
Shravani <shramore58@gmail.com>
2023-02-06 19:50:19 +0530

    Added: Subtraction is added

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
 class Calculator:
     def addition(num1, num2):
         return num1 + num2
+
+    def subtraction(num1, num2):
+        return num1 - num2
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from calculator import Calculator
 
-op = int(input('Enter the operation\n1.Addition\n'))
+op = int(input('Enter the operation\n1.Addition\n2.Subtraction\n'))
 num1, num2 = list(map(int, input('Enter two numbers: ').split(',')))
 
-print(Calculator.addition(num1, num2))
+if op == 1:
+    print(Calculator.addition(num1, num2))
+elif op == 2:
+    print(Calculator.subtraction(num1, num2))
